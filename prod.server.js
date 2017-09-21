2cfb4c5d6a2c3c83d9c634ff330c75fbe5d2c5ef
tlx6550 <912730337@qq.com>
2017-09-21 11:16:05 +0800

    Update prod.server.js

--- prod.server.js
+++ prod.server.js
@@ -1,8 +1,8 @@
 var express = require('express')
-var config = require('./config/index')
+/*var config = require('./config/index')*/
 var axios = require('axios')
 
-var port = process.env.PORT || config.build.port
+var port = process.env.PORT || 5000
 
 var app = express()
 
